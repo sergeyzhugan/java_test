ea709420cea26b675870e80c38420d1422d0f16d
Ser Zhu <sergey80-ne@yandex.ua>
2018-03-28 22:40:14 +0300

    code for new classses

--- sendbox/src/main/java/ru/st/p/sendbox/MyFirstProg.java
+++ sendbox/src/main/java/ru/st/p/sendbox/MyFirstProg.java
@@ -2,6 +2,17 @@ package ru.st.p.sendbox;
 
 public class MyFirstProg {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+//    System.out.println("Hello world!");
+
+    double l = 5;
+    Square s = new Square(l);
+    System.out.println("Площадь квадрата со стороной " +
+            l +" = " + s.area());
+
+    double a = 7;
+    Rectangle r = new Rectangle(a,l);
+    System.out.println("Площадь прямоугольника со сторонами " +
+            a + " и " + l +" = " + r.area());
+
   }
 }
