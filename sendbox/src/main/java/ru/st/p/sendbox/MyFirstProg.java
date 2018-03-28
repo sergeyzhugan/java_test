026a1559419ffb4a9659e37660a3c2da0ce8ddfd
Ser Zhu <sergey80-ne@yandex.ua>
2018-03-28 22:02:04 +0300

    create new package

--- sendbox/src/main/java/ru/st/p/sendbox/MyFirstProg.java
+++ sendbox/src/main/java/ru/st/p/sendbox/MyFirstProg.java
@@ -1,5 +1,7 @@
+package ru.st.p.sendbox;
+
 public class MyFirstProg {
-	public static void  main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
 }
