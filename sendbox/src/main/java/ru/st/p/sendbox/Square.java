a1c2653873f68780cbf821d940127b72d388f06d
Ser Zhu <sergey80-ne@yandex.ua>
2018-03-28 22:26:44 +0300

    create Rectangle class

---
+++ sendbox/src/main/java/ru/st/p/sendbox/Square.java
@@ -0,0 +1,12 @@
+package ru.st.p.sendbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+  public double area(){
+    return this.l * this.l;
+  }
+}
