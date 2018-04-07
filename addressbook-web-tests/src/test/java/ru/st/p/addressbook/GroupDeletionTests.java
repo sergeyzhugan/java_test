3b24240ed953c2b3c6adf8adf43c83623912ea85
Ser Zhu <sergey80-ne@yandex.ua>
2018-04-07 15:23:05 +0200

    Revert "fix clean"
    This reverts commit be95c4728164d304a6ea0ef5fa89583a8b9a6c69.

--- addressbook-web-tests/src/test/java/ru/st/p/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/st/p/addressbook/GroupDeletionTests.java
@@ -1,15 +1,61 @@
 package ru.st.p.addressbook;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
-public class GroupDeletionTests extends TestBase {
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
 
+public class GroupDeletionTests {
+    ChromeDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
     @Test
     public void testGroupDeletion() {
-        gotoGroupPage();
-        selectGroup();
-        deleteSelectedGroup();
-        returnToGroupPage();
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).sendKeys("\\undefined");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.linkText("GROUPS")).click();
+        if (!wd.findElement(By.name("selected[]")).isSelected()) {
+            wd.findElement(By.name("selected[]")).click();
+        }
+        wd.findElement(By.name("delete")).click();
+        wd.findElement(By.linkText("group page")).click();
     }
-
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 }
